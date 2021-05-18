b7db7f7c23b94da15c20ee382f6fa43d8c911a47
unknown <jbj1996@gmail.com>
2021-05-18 16:47:37 -0400

    added SearchParams and integrated it to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams"
 
 const App = () => {
   return React.createElement(
   React.createElement("h1", {}, "Adopt Me!"),
-  React.createElement(Pet, {
-    name: "Jack",
-    animal: "Dog",
-    personality: "Slobber-Monger"
-  }),
-  React.createElement(Pet, {
-    name: "Rosie",
-    animal: "Bird",
-    personality: "evil",
-  }),
-  React.createElement(Pet, {name: "Kronk", animal: "Cat", personality: "Spacey-Loner"}),
+  <SearchParams />
   );
 };
 
