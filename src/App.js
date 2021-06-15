82802fbe6453a6197703d12d11fc95bea6202e45
unknown <jbj1996@gmail.com>
2021-06-15 12:59:21 -0400

    altered format settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal), 
+    React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.type),
-  ])
-}
-
-const App = () => {
-  return React.createElement(
-      "div",
-      {},
-      [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name:"Mia", animal:"Cat", type:"WOBBLY"}),
-      React.createElement(Pet, { name:"Bandit", animal:"Cat", type:"MOOOOO"}),
-      React.createElement(Pet, { name:"Amber", animal:"Cat", type:"CHoNK"}),
-      ]
-      
-  );
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Mia", animal: "Cat", type: "WOBBLY" }),
+    React.createElement(Pet, { name: "Bandit", animal: "Cat", type: "MOOOOO" }),
+    React.createElement(Pet, { name: "Amber", animal: "Cat", type: "CHoNK" }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
