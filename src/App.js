068a3a677142a87b90095b9ad43c9b7287704995
unknown <jbj1996@gmail.com>
2021-06-16 12:11:35 -0400

    converted app.js to jsx

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet'
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Mia", animal: "Cat", type: "WOBBLY" }),
-    React.createElement(Pet, { name: "Bandit", animal: "Cat", type: "MOOOOO" }),
-    React.createElement(Pet, { name: "Amber", animal: "Cat", type: "CHoNK" }),
-  ]);
-};
+//Raw REACT
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, { name: "Mia", animal: "Cat", type: "WOBBLY" }),
+//     React.createElement(Pet, { name: "Bandit", animal: "Cat", type: "MOOOOO" }),
+//     React.createElement(Pet, { name: "Amber", animal: "Cat", type: "CHoNK" }),
+//   ]);
+// };
 
+const App = () => {
+  return(
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Mia" animal="Cat" type="wObBlY" />
+      <Pet name="Bandit" animal="Cat" type="MOOOO" />
+      <Pet name="Amber" animal="Cat" type="CHoNK" />
+    </div>
+  )
+}
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
